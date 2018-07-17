f25a575adfbfeaaaf50c7e2b71fda88095c64dd5
Elena Novikova <36162105+ElenaNovikova@users.noreply.github.com>
2018-07-17 23:34:46 +0100

    feat: Add aria attributes

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -70,13 +70,16 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
 
   const address = document.getElementById('restaurant-address');
   address.innerHTML = restaurant.address;
+  address.setAttribute('aria-label', 'Address: ' + restaurant.address);
 
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.setAttribute('alt', 'Photo of ' + restaurant.name + ' restaurant')
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
+  cuisine.setAttribute('aria-label', 'Cuisine: ' + restaurant.cuisine_type);
 
   // fill operating hours
   if (restaurant.operating_hours) {
