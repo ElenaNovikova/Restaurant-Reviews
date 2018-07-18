6fba2f0f9f5ddbaead3178b913c810668bea25c8
Elena Novikova <36162105+ElenaNovikova@users.noreply.github.com>
2018-07-18 10:48:02 +0100

    fix: Add aria-label "breadcrumb"

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -163,6 +163,7 @@ fillBreadcrumb = (restaurant=self.restaurant) => {
   const li = document.createElement('li');
   li.innerHTML = restaurant.name;
   breadcrumb.appendChild(li);
+  breadcrumb.setAttribute('aria-label', 'Breadcrumb');
 }
 
 /**
